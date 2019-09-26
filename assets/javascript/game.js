2e728dbd2b36b25fedabcf380f94e897d2b5b712
Jawed Ekhlas <jawz07r1@gmail.com>
2019-09-26 13:44:17 -0700

    https://github.com/Jawed11/nit-4-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 const renderGems = () => {
     document.getElementById('gems').innerHtML =''
- for (let i = 0; i < 3; i++) {
-     const random = Math.floor(Math.random() * 20) +1
+ for(let i = 0; i < 3; i++) {
+     const random = Math.floor(Math.random() * 30) +1
    let gemElem = document.createElement('div')
     gemElem.className = 'co s4'
     gemElem.innnerHTML = `
     div class = "card" data->value= "${random} >
     div class = "card-image">
-    <img src ="./gems${i + 1}">
+     <img src ="./gem${i + 1}">.png"
     </div>
     </div>
     
@@ -17,7 +17,6 @@ const renderGems = () => {
   }
 }
 
-renderGems()
 
 console.log(renderGems)
 
